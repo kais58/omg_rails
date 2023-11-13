e603a55802596c808a3961b2a2234ed482169490
Callum Massey <kais58@sucs.org>
2023-11-13 01:05:37 +0000

    Update 20231112234706_add_elo_to_players.rb

--- db/migrate/20231112234706_add_elo_to_players.rb
+++ db/migrate/20231112234706_add_elo_to_players.rb
@@ -2,7 +2,8 @@ class AddEloToPlayers < ActiveRecord::Migration[6.1]
   def change
     change_table :players do |t|
       t.integer  :elo, default: 1500, null: false
-      t.decimal  :sigma, default: 0, null: false
+      t.integer  :rd, default: 350, null: false
+      t.float    :vol, default: 0.06, null: false
     end
   end
 end
